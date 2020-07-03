7a1dd193d880a6887fdc01e9c1916c7d4a1c2ced
Yenying-Ho <a26782871@gmail.com>
2020-07-03 16:16:09 +0800

    5

---
+++ recipe/recipe_home.js
@@ -0,0 +1,95 @@
+// home  &  list
+$("#light").on("click", function(){
+    window.localStorage.removeItem("option")
+    window.localStorage.setItem("option", "light")
+    window.location.href =  "/recipe/recipe_list.html"
+    console.log($("#light").attr("id"))
+})
+$("#fit").on("click", function(){
+    window.localStorage.removeItem("option")
+    window.localStorage.setItem("option", "fit")
+    window.location.href =  "/recipe/recipe_list.html"
+    console.log($("#fit").attr("id"))
+})
+$("#vega").on("click", function(){
+    window.localStorage.removeItem("option")
+    window.localStorage.setItem("option", "vega")
+    window.location.href =  "/recipe/recipe_list.html"
+    console.log($("#vega").attr("id"))
+})
+$("#meat").on("click", function(){
+    window.localStorage.removeItem("option")
+    window.localStorage.setItem("option", "meat")
+    window.location.href =  "/recipe/recipe_list.html"
+    console.log($("#meat").attr("id"))
+})
+$("#drinks").on("click", function(){
+    window.localStorage.removeItem("option")
+    window.localStorage.setItem("option", "drinks")
+    window.location.href =  "/recipe/recipe_list.html"
+    console.log($("#drinks").attr("id"))
+})
+$("#soup").on("click", function(){
+    window.localStorage.removeItem("option")
+    window.localStorage.setItem("option", "soup")
+    window.location.href =  "/recipe/recipe_list.html"
+    console.log($("#soup").attr("id"))
+})
+
+//home
+
+//list
+if(localStorage.getItem("option") == "light"){
+    $("#light").css("border","black 2px solid")
+    $(".light").removeClass("d-none")
+    $(".fit").addClass("d-none")
+    $(".vega").addClass("d-none")
+    $(".meat").addClass("d-none")
+    $(".drinks").addClass("d-none")
+    $(".soup").addClass("d-none")
+}
+if(localStorage.getItem("option") == "fit"){
+    $("#fit").css("border","black 2px solid")
+    $(".light").addClass("d-none")
+    $(".fit").removeClass("d-none")
+    $(".vega").addClass("d-none")
+    $(".meat").addClass("d-none")
+    $(".drinks").addClass("d-none")
+    $(".soup").addClass("d-none")
+}
+if(localStorage.getItem("option") == "vega"){
+    $("#vega").css("border","black 2px solid")
+    $(".light").addClass("d-none")
+    $(".fit").addClass("d-none")
+    $(".vega").removeClass("d-none")
+    $(".meat").addClass("d-none")
+    $(".drinks").addClass("d-none")
+    $(".soup").addClass("d-none")
+}
+if(localStorage.getItem("option") == "meat"){
+    $("#meat").css("border","black 2px solid")
+    $(".light").addClass("d-none")
+    $(".fit").addClass("d-none")
+    $(".vega").addClass("d-none")
+    $(".meat").removeClass("d-none")
+    $(".drinks").addClass("d-none")
+    $(".soup").addClass("d-none")
+}
+if(localStorage.getItem("option") == "drinks"){
+    $("#drinks").css("border","black 2px solid")
+    $(".light").addClass("d-none")
+    $(".fit").addClass("d-none")
+    $(".vega").addClass("d-none")
+    $(".meat").addClass("d-none")
+    $(".drinks").removeClass("d-none")
+    $(".soup").addClass("d-none")
+}
+if(localStorage.getItem("option") == "soup"){
+    $("#soup").css("border","black 2px solid")
+    $(".light").addClass("d-none")
+    $(".fit").addClass("d-none")
+    $(".vega").addClass("d-none")
+    $(".meat").addClass("d-none")
+    $(".drinks").addClass("d-none")
+    $(".soup").removeClass("d-none")
+}
