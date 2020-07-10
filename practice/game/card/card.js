9806e01537a7cb8a3494d3223af5ff1068142e24
Yenying-Ho <a26782871@gmail.com>
2020-07-10 16:27:17 +0800

    5

--- practice/game/card/card.js
+++ practice/game/card/card.js
@@ -12,13 +12,20 @@ for (var i = 0; i < ranNum; i++) {
 
 $(".cards-back").each(function(){
     for(var i = 0; i < 24; i++){
-        document.getElementsByClassName("cards-back")[i].style.background = "#ffffff url(/practice/game/card/images/"+ result[i] +".png) center";
+        document.getElementsByClassName("cards-back")[i].style.background = "#ffffff url(../card/images/"+ result[i] +".png) center";
         document.getElementsByClassName("cards-back")[i].style.backgroundSize = "cover";
     }
 })
 var optionA = []
-
+var times = 0
+var time = setInterval(() => {
+    $(".time").text(time.toFixed(2))
+    time += 0.1
+}, 100);
+var count = 0;
 $(".cards-head").click(function(){
+    times++
+    // console.log(times)
     $(this).css({"transform":"rotateY(180deg)",
                     "opacity":"0",
                     "z-index":"-1"})
@@ -27,22 +34,24 @@ $(".cards-head").click(function(){
                     "z-index":"1"})
     $(this).attr("data-open", "t")
     $(this).next(".cards-back").attr("data-open", "t")
-    console.log($(this).attr("data-open"))                
+    // console.log($(this).attr("data-open"))                
     optionA.push($(this).next(".cards-back").css("background-image"))
-    console.log(optionA[0])
-    console.log(optionA[1])
+    // console.log(optionA[0])
+    // console.log(optionA[1])
     if(optionA.length > 1){
         if(optionA[0] == optionA[1]){
             // $(this).css({"opacity":"0"})
             // $(this).next(".cards-back").css({"opacity":"0"})
-            setTimeout(function(){
-                $(".card-box").find("[data-open = 't']").css({"display":"none"})
-            },800)
-            console.log("一樣")
             optionA = [];
-
+            setTimeout(function(){
+                $(".card-box").find("[data-open = 't']").css({"opacity":"0"})
+                $(".card-box").find("[data-open = 't']").removeClass("cards-head","cards-back")
+            },800)
+            // console.log("一樣")
+            count++
         }else{
-            console.log("不一樣")
+            // console.log("不一樣")
+            optionA = [];
             setTimeout(function(){
                 $(".cards-head").css({"transform":"rotateY(0deg)",
                                       "opacity":"1",
@@ -53,19 +62,22 @@ $(".cards-head").click(function(){
                 $(".card-box").find("[data-open = 't']").attr("data-open", "f") 
             },800)
 
-            optionA = [];
         }
-    }else{
-        
+    }
+    $(".times").text(Math.round(times/2))
+    // console.log($(".card-box").find("[data-open = 't']").length)
+
+    if(count == 12){
+        console.log("End")
+        clearInterval(time)
+        $(".point").text(Math.round(1000-times*4-time))
+        $(".time").removeClass("time")
     }
 
 })
-// $(".cards-back").click(function(){
-//     $(this).css({"transform":"rotateY(0deg)",
-//                     "opacity":"0",
-//                     "z-index":"-1"})
-//     $(this).prev(".cards-head").css({"transform":"rotateY(0deg)",
-//                     "opacity":"1",
-//                     "z-index":"1"})
-//     $(this).prev(".cards-head").attr("data-open", "f")               
+// $(document).ready(function(){
+//     if($(".card-box").find("[data-open = 't']").length == 0){
+//         alert("Game Over")
+//     }
 // })
+
