492b0c5b4b18e294c9eb135af6af46e617f9255e
Yenying-Ho <a26782871@gmail.com>
2020-07-13 10:23:29 +0800

    u-bike

---
+++ practice/u-bike/u-bike.js
@@ -0,0 +1,57 @@
+fetch('https://script.googleusercontent.com/macros/echo?user_content_key=C1hU2sDZcxImNfhKczMdiZwsxixRD4IfRPmoNXAkNr1_9meUzCw6sT1BvRavQXG01hH5i5Ky0bqoglGD9EdUSg3q8_t21uaPOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa2nCQdzj0xaveqj_aTMGhcOgACOoWSznLeycE0b6pUjYUSBIj_zmygdTizpQPW66RJmYY5VWNeg0SXadqjNzSGMoMmxOzy6jE9XbhKgcWebsKkourV1Kaoz58c6zYdGFC3PMXlJAeqAFrEwo82IiTMAixelIRi4nAMaNHffJA7gmOx5xLD8uBuDznnLJsP_IYY6Yu90O9CkL&lib=MU7uPymDDbcE3lDdK1Ircj_pRVCifQbmI')
+.then(res => {
+    // 處理 response
+    return res.json();
+}).then(result => {
+    let str = '';
+    let list = document.querySelector('.table_list');
+    let arrLength = 10;
+    for(let i = 0; i < arrLength; i++){
+        str += `    <tr data-lat="${result[i].lat}" data-lng="${result[i].lng}">
+                        <td class="sno">${result[i].sno}</td>
+                        <td class="sna">${result[i].sna}</td>
+                        <td class="tot">${result[i].tot}</td>
+                        <td class="sbi">${result[i].sbi}</td>
+                        <td class="ar">${result[i].ar}</td>
+                        <td class="bemp">${result[i].bemp}</td>
+                    </tr>`
+    }
+    list.innerHTML = str;
+    $(document).on("click", ".more", function(){
+        arrLength += 10;
+        str = '';
+        for(let i = 0; i < arrLength; i++){
+            str += `    <tr data-lat="${result[i].lat}" data-lng="${result[i].lng}">
+                            <td class="sno">${result[i].sno}</td>
+                            <td class="sna">${result[i].sna}</td>
+                            <td class="tot">${result[i].tot}</td>
+                            <td class="sbi">${result[i].sbi}</td>
+                            <td class="ar">${result[i].ar}</td>
+                            <td class="bemp">${result[i].bemp}</td>
+                        </tr>`
+        }
+    list.innerHTML = str;
+    // console.log(arrLength)
+    $(".table_list tr").on("click",function(){
+        lat = $(this).attr("data-lat")
+        lng = $(this).attr("data-lng")
+        console.log(lat)
+        console.log(lng)
+        window.location.href = "https://www.google.com.tw/maps/search/" + lat + "," + lng;
+    })
+    })
+    // console.log(nameArr)
+    // console.log(latArr)
+    // console.log(lngArr)
+    // console.log(locArr)
+    // console.log(arrLength)
+    $(".table_list tr").on("click",function(){
+        lat = $(this).attr("data-lat")
+        lng = $(this).attr("data-lng")
+        console.log(lat)
+        console.log(lng)
+        window.location.href = "https://www.google.com.tw/maps/search/" + lat + "," + lng;
+    })
+}).catch(function(err) {
+    // 錯誤處理
+});
