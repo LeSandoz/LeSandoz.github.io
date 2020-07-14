f9b3d7b6ebcfb68439618132bd1aa157524b818d
Yenying-Ho <a26782871@gmail.com>
2020-07-14 15:12:05 +0800

    5

--- practice/mask-map/mask-map.js
+++ practice/mask-map/mask-map.js
@@ -1,6 +1,7 @@
 let map;
 let marker = [];
 let position = [];
+let infowindow = [];
 
 // console.log(position)
 fetch('https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json')
@@ -20,7 +21,7 @@ fetch('https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.jso
                   + result.features[i].properties.address + "\n"
                   + "成人口罩數量: " + result.features[i].properties.mask_adult + "\n"
                   + "兒童口罩數量: " + result.features[i].properties.mask_child + "\n"
-        
+        let content = title
         position.push({title: title,
                        lat: result.features[i].geometry.coordinates[1],
                        lng: result.features[i].geometry.coordinates[0]})
@@ -37,7 +38,17 @@ function initMap() {
         lng: 121.3802776
       }
     });
+
+    var marker1= new google.maps.Marker({
+        map: map,
+        position: {lat: 24.9456985, lng: 121.3802776},
+        title: "Sunny"
+    });
+    var contentString = '<h1>123</h1>';
     
+
+
+    // for (let i = 0; i < 30; i++) {
     for (let i = 0; i < position.length; i++) {
         marker[i] = new google.maps.Marker({
             position: {
@@ -48,19 +59,25 @@ function initMap() {
             title: position[i].title,
 
           });
-          
+
+        infowindow[i] = new google.maps.InfoWindow({
+            content: position[i].title,
+            position: {
+                lat: position[i].lat,
+                lng: position[i].lng
+            },
+            maxWidth: 200,
+            pixelOffset: new google.maps.Size(100, -20) 
+        });
+        // infowindow.open(map);
+        // console.log(infowindow)
+        marker[i].addListener('click',function(){
+            console.log(infowindow[i])
+            infowindow[i].open(map,marker[i]);
+        });
     }
-    // google.maps.event.addListener(marker, function(){
-    //     console.log("click")
-    // })
-    marker.addListener('click',function(){
-        if(marker.getAnimation() == null){
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-        }else{
-          marker.setAnimation(null);
-        }
-    });
-    
+
+
 
 
   }
@@ -83,7 +100,20 @@ function initMap() {
 
 
 
-
+$(document).on("click",".left-arrow", function(){
+    console.log(123)
+    $(".side-bar").css("width", 0)
+    $(".update").addClass("d-none")
+    $(".left-arrow").addClass("d-none")
+    $(".back").removeClass("d-none")
+})
+$(document).on("click",".back", function(){
+    console.log(123)
+    $(".side-bar").css("width", "20%")
+    $(".update").removeClass("d-none")
+    $(".left-arrow").removeClass("d-none")
+    $(".back").addClass("d-none")
+})
 
 
 
