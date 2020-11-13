929d65728013b3cd335cb48d898867182a99b4fc
Yenying-Ho <a26782871@gmail.com>
2020-11-13 16:26:34 +0800

    中英切換

--- 智品/index.js
+++ 智品/index.js
@@ -1,3 +1,13 @@
 // $(".start_your_project").on("click", function(){
 //     window.location.href ="https://driveuploader.com/upload/P3bXC1HZlF/";
 // })
+$(".zh-tw").css("display", "none");
+
+$(".language1").on("click", function(){
+    $(".zh-tw").css("display", "");
+    $(".en").css("display", "none");
+})
+$(".language2").on("click", function(){
+    $(".en").css("display", "");
+    $(".zh-tw").css("display", "none");
+})
