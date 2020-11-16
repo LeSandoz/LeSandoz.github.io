e000a263a64c6afa5c664a9b371abe0ea3619a0a
Yenying-Ho <a26782871@gmail.com>
2020-11-16 11:33:25 +0800

    0

--- 智品/index.js
+++ 智品/index.js
@@ -11,3 +11,13 @@ $(".language2").on("click", function(){
     $(".en").css("display", "");
     $(".zh-tw").css("display", "none");
 })
+$(document).on("click", ".menu", function(){
+    $(".header_list_bot_1000").css("display", "block");
+    $(".menu").addClass("clickOn");
+});
+$(document).on("click", ".clickOn", function(){
+    $(".header_list_bot_1000").css("display", "none");
+    $(".menu").removeClass("clickOn");
+});
+
+
