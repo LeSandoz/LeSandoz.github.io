ef49de6fc389e8b0067cd72be38b7919a592ec46
Yenying-Ho <a26782871@gmail.com>
2020-11-25 15:13:43 +0800

    aribin

--- practice/game/slots/slots2.js
+++ practice/game/slots/slots2.js
@@ -10,6 +10,7 @@ var i = '';
             var id2 = Math.round(Math.random()*(shape.length - 1));
             var id3 = Math.round(Math.random()*(locations.length - 1));
             var id4 = Math.round(Math.random()*(fullname.length - 1));
+            var colorHtml = '';
             i++;
             // var name = $("#name").val();
             // console.log(id1);
@@ -27,11 +28,32 @@ var i = '';
                 // $(".fullname").text(fullname[id4]);
                 // console.log(fullname[id4]);
                 // $("#name option[value=" + name + "]").remove();
+                if(color[id1] == "藍"){
+                    colorHtml = `<td width="12%" style="color: skyblue;">${color[id1]}</td>`;
+                }else if(color[id1] == "紅"){
+                    colorHtml = `<td width="12%" style="color: red;">${color[id1]}</td>`;               
+                }else if(color[id1] == "橙"){
+                    colorHtml = `<td width="12%" style="color: orange;">${color[id1]}</td>`;               
+                }else if(color[id1] == "黃"){
+                    colorHtml = `<td width="12%" style="color: yellow;">${color[id1]}</td>`;               
+                }else if(color[id1] == "綠"){
+                    colorHtml = `<td width="12%" style="color: green;">${color[id1]}</td>`;               
+                }else if(color[id1] == "靛"){
+                    colorHtml = `<td width="12%" style="color: blue;">${color[id1]}</td>`;               
+                }else if(color[id1] == "紫"){
+                    colorHtml = `<td width="12%" style="color: purple;">${color[id1]}</td>`;               
+                }else if(color[id1] == "黑"){
+                    colorHtml = `<td width="12%" style="color: black;">${color[id1]}</td>`;               
+                }else if(color[id1] == "白"){
+                    colorHtml = `<td width="12%" style="color: white; background: black;">${color[id1]}</td>`;               
+                }else if(color[id1] == "灰"){
+                    colorHtml = `<td width="12%" style="color: gray;">${color[id1]}</td>`;               
+                }
                 document.querySelector("#box").innerHTML +=
                 `   <tr>
                         <td width="12%">${i}</td>
                         <td width="12%">${fullname[id4]}</td>
-                        <td width="12%">${color[id1]}</td>
+                        ${colorHtml}
                         <td width="12%">${shape[id2]}</td>
                         <td width="12%">${locations[id3]}</td>
                         <td width="40%">${fullname[id4]} 要準備的是用在 ${locations[id3]}的 ${color[id1]}色 ${shape[id2]}東西</td>
