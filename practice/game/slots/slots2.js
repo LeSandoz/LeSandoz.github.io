65ddb6d09a3fdc5015555756b2b310b6691adfa1
Yenying-Ho <a26782871@gmail.com>
2020-11-25 14:22:37 +0800

    0

--- practice/game/slots/slots2.js
+++ practice/game/slots/slots2.js
@@ -2,13 +2,15 @@ var color = ['都可','紅','橙','黃','綠','藍','靛','紫','黑','白','灰
 var shape = ['都可','圓形','固體','方形','六角形','五角形','不規則','三角形','圓柱','液體','長條型'];
 var locations = ['都可','廚房','廁所','辦公室','房間','客廳','陽台','健身房','室外','教室','球場'];
 var fullname = ['何彥穎','王嘉禾','劉怡婷','方建哲','蔡亞筑','呂學承','蔡承璋','黃宣燁','曾文瑀','陳子秝'];
+var i = 0;
+
     $(document).on("click", ".button", function(){
         // for(let i = 0; i < 10; i++){
             var id1 = Math.round(Math.random()*(color.length - 1));
             var id2 = Math.round(Math.random()*(shape.length - 1));
             var id3 = Math.round(Math.random()*(locations.length - 1));
             var id4 = Math.round(Math.random()*(fullname.length - 1));
-
+            i++;
             // var name = $("#name").val();
             // console.log(id1);
             // console.log(id2);
@@ -27,10 +29,11 @@ var fullname = ['何彥穎','王嘉禾','劉怡婷','方建哲','蔡亞筑','呂
                 // $("#name option[value=" + name + "]").remove();
                 document.querySelector("#box").innerHTML +=
                 `   <tr>
-                        <td width="15%">${fullname[id4]}</td>
-                        <td width="15%">${color[id1]}</td>
-                        <td width="15%">${shape[id2]}</td>
-                        <td width="15%">${locations[id3]}</td>
+                        <td width="12%">${i}</td>
+                        <td width="12%">${fullname[id4]}</td>
+                        <td width="12%">${color[id1]}</td>
+                        <td width="12%">${shape[id2]}</td>
+                        <td width="12%">${locations[id3]}</td>
                         <td width="40%">${fullname[id4]} 要準備的是用在 ${locations[id3]}的 ${color[id1]}色 ${shape[id2]}東西</td>
                     </tr>
                 `;
